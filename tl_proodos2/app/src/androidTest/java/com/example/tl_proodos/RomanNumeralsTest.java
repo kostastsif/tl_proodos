fc915da56dfb820931ad9f68fa4db76e17165bed
kostastsif <73225991+kostastsif@users.noreply.github.com>
2020-12-14 19:16:21 +0200

    tl_proodos
    Συγνώμη για την καθυστέρηση δασκαλέ
    Καλή συνέχεια

---
+++ tl_proodos2/app/src/androidTest/java/com/example/tl_proodos/RomanNumeralsTest.java
@@ -0,0 +1,15 @@
+package com.example.tl_proodos;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RomanNumeralsTest {
+
+    @Test
+    public void testRomanNumerals() {
+        for (int i = 1; i < 256; i++) {
+            System.out.println("i=" + i + " -> " + RomanNumerals(i));
+        }
+    }
+}
